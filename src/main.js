6aff751174f60fbae77964af5aadf87664d871bd
Greentea <wangyukun@gmail.com>
2018-07-04 10:15:33 +0800

    project init common

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
---
+++ src/page/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+    hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped rel="stylesheet/stylus">
+
+</style>
